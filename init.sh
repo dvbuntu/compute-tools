f08e4513fc1c3feef4423cefdb820c626004e357
Dan Van Boxel <the.dvb@gmail.com>
2015-07-11 20:27:15 -0400

    Add init script...
    AUR helpers don't help much if I can't supply a custom file

---
+++ init.sh
@@ -0,0 +1,51 @@
+#! /bin/bash
+
+# move to Downloads
+install -d $HOME/Downloads
+cd $HOME/Downloads
+
+# First install package-query
+curl -O https://aur.archlinux.org/packages/pa/package-query/package-query.tar.gz
+tar -xzvf package-query.tar.gz
+cd package-query
+makepkg -si
+sudo pacman -U package-query*.tar.xz
+cd ..
+
+# now install yaourt
+curl -O https://aur.archlinux.org/packages/ya/yaourt/yaourt.tar.gz
+tar -xzvf yaourt.tar.gz
+cd yaourt
+makepkg -si
+sudo pacman -U yaourt*.tar.xz
+cd ..
+
+# run yaourt on the 
+pkgs = (
+         'xf86-video-vesa'
+         'base-devel'
+         'sudo'
+         'git'
+         'tmux'
+         'python3'
+         'ipython'
+         'python-numpy'
+         'vim'
+         'openbox'
+         'zsh'
+         )
+aur_pkgs = (
+         'oh-my-zsh-git'
+         'slimux-git'
+         )
+
+sudo pacman -S
+for i in "${pkgs[@]}"
+do
+    sudo pacman -y $i
+done
+
+for i in "${aur_pkgs[@]}"
+do
+    yaourt $i
+done
